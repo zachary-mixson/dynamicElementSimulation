1e53bdf3e1f98b8fb2be8bf8d3eb7b2398bf0b33
zachary mixson <48222554+zachary-mixson@users.noreply.github.com>
2022-12-12 20:19:08 -0500

    Update elementCreator.h

--- elementCreator.h
+++ elementCreator.h
@@ -4,15 +4,15 @@
 #include "waterElement.h"
 #include "sandElement.h"
 #include "earthElement.h"
-class ElementCreator 
+class ElementCreator
 {
 public:
 
 	virtual ~ElementCreator() {};
 	virtual Element* FactoryMethod() const = 0;
+
 private:
 
 
 
 };
-
